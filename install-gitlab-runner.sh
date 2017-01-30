41ed173ff0311e057c05926179e8b08a5d376cb1
Pavol Noha <pavol.noha@gmail.com>
2017-01-30 08:41:34 +0100

    cleanup -> gitlab service url as property in stack file

--- install-gitlab-runner.sh
+++ install-gitlab-runner.sh
@@ -3,6 +3,7 @@
 echo "Enter gitlab runner registration token:"
 read gitlab_runner_token
 
+export GITLAB_SERVICE_URL=http://ci_gitlab
 export GITLAB_RUNNER_TOKEN=${gitlab_runner_token}
 
 docker stack deploy -c stack/gitlab-runner-stack.yml ci
