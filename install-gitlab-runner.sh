dbc80da388396ebd3a3d552b003a783cbeb002c6
root <root@swarm-manager000000.rd4hk3fx2eruxnji4erq0aj4hb.ax.internal.cloudapp.net>
2017-02-01 13:09:40 +0000

    gitlab host added into runner script, cleanup

--- install-gitlab-runner.sh
+++ install-gitlab-runner.sh
@@ -3,7 +3,8 @@
 echo "Enter gitlab runner registration token:"
 read gitlab_runner_token
 
-export GITLAB_SERVICE_URL=http://ci_gitlab
+export GITLAB_HOST=ci_gitlab
+export GITLAB_IP=$(hostname -i)
 export GITLAB_RUNNER_TOKEN=${gitlab_runner_token}
 
 docker stack deploy -c stack/gitlab-runner-stack.yml ci
